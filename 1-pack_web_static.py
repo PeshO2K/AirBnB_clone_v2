4dc960af83ab4d0011d6bf1b047a695833753986
PeshO2K <patienceotuke@gmial.com>
2024-01-27 00:27:54 +0300

    Pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
 """ Fabric script for creating archive from github repos"""
+
 from fabric.api import local
 from datetime import datetime
 import os
 
-
-# def do_pack():
-#     """creates a tgz archive from the current folder"""
-#     local("mkdir -p versions")
-#     out_name = f"web_static_{datetime.now().strftime('%Y%m%d%H%M%S')}.tgz"
-#     result = local("tar -cvzf versions/{} web_static".format(out_name),
-#                    capture=True)
-#     if not result.return_code:
-#         return f"versions/{out_name}"
-#     return None
 
 def do_pack():
     """Creates a .tgz archive from the contents of the web_static folder.
@@ -28,7 +19,8 @@ def do_pack():
     archive_name = f"web_static_{timestamp}.tgz"
 
     # Compress the web_static folder into the archive
-    exit_code = local(f"tar -cvzf versions/{archive_name} web_static", capture=True)
+    exit_code = local(f"tar -cvzf versions/{archive_name} web_static",
+                      capture=True)
     if exit_code.return_code:
         return None
     return os.path.join("versions", archive_name)
