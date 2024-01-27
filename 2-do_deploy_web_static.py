d038e44d85313acc65fae0fb29ddac1d7a07fc9b
PeshO2K <patienceotuke@gmial.com>
2024-01-27 12:40:49 +0300

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,73 +4,55 @@ Fabric script for distributing an archive to web servers
 """
 
 from fabric.api import env, run, put
-from os.path import exists
-from datetime import datetime
+from os import path
 
 env.hosts = ['18.204.14.158', '54.82.197.53']
 
-
-# def do_deploy(archive_path):
-#     """
-#     Distributes an archive to web servers
-#     """
-#     if not exists(archive_path):
-#         return False
-
-#     try:
-#         # Upload the archive to the /tmp/ directory of the web server
-#         put(archive_path, "/tmp/")
-
-#         # Extract the archive to the folder /data/web_static/releases/<archive
-#         # filename without extension>
-#         archive_filename = archive_path.split("/")[-1]
-#         folder_name = archive_filename.split(".")[0]
-#         run("mkdir -p /data/web_static/releases/{}".format(folder_name))
-#         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-#             .format(archive_filename, folder_name))
-
-#         # Delete the archive from the web server
-#         run("rm /tmp/{}".format(archive_filename))
-
-#         # Delete the symbolic link /data/web_static/current from the web server
-#         run("rm -rf /data/web_static/current")
-
-#         # Create a new symbolic link /data/web_static/current linked to
-#         # the new version of the code
-#         run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-#             .format(folder_name))
-#         return True
-#     except Exception as e:
-#         return False
-
-
-# env.hosts = [
-#     '3.85.54.241',
-#     '52.86.86.171'
-# ]
-
-
 def do_deploy(archive_path):
-    """distributes an archive to webservers"""
-    if not os.path.exists(archive_path):
+    """
+    Distributes an archive to web servers
+
+    Args:
+        archive_path (str): Path to the archive file
+
+    Returns:
+        True if all operations have been done correctly, otherwise returns False
+    """
+    # Check if the archive file exists
+    if not path.exists(archive_path):
         return False
 
-    basename = os.path.basename(archive_path)
-    rem_archive_path = f"/tmp/{basename}"
+    # Extract information from the archive path
+    archive_filename = path.basename(archive_path)
+    folder_name = path.splitext(archive_filename)[0]
+    remote_archive_path = f"/tmp/{archive_filename}"
 
     try:
-        put(archive_path, rem_archive_path)
-        x_archive = "/data/web_static/releases/{}".format(
-            os.path.splitext(basename)[0]
-        )
-        run(f"mkdir -p {x_archive}")
-        run("tar -xzf {} -C {} --strip-components=1".format(
-            rem_archive_path, x_archive
-        ))
-        run(f"rm -f {rem_archive_path}")
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, remote_archive_path)
+
+        # Define the target extraction directory on the web server
+        target_extraction_path = f"/data/web_static/releases/{folder_name}"
+
+        # Create the target directory on the web server
+        run("mkdir -p {}".format(target_extraction_path))
+
+        # Extract the archive, stripping the top-level directory
+        run("tar -xzf {} -C {} --strip-components=1"
+            .format(remote_archive_path, target_extraction_path))
+
+        # Delete the uploaded archive from the web server
+        run("rm -f {}".format(remote_archive_path))
+
+        # Remove the previous symbolic link
         symlink = "/data/web_static/current"
         run(f"rm -rf {symlink}")
-        run(f"ln -s {x_archive}/ {symlink}")
+
+        # Create a new symbolic link linked to the extracted version
+        run("ln -s {}/ {}"
+            .format(target_extraction_path, symlink))
+
+        return True
+
     except Exception:
         return False
-    return True
