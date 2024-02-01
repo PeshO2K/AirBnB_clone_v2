64b010230d25bf65b1e9ab42a92ce26bdffb700b
PeshO2K <patienceotuke@gmial.com>
2024-02-01 03:51:42 +0300

    New script

--- 2-do_deploy_web_static copy.py
+++ 2-do_deploy_web_static copy.py
@@ -7,8 +7,6 @@ from fabric.api import env, run, put
 # from os import path
 import os
 
-env.user = "ubuntu"
-env.key_filename = "~/.ssh/my_school"
 env.hosts = ['18.204.14.158', '54.82.197.53']
 
 # def do_deploy(archive_path):
@@ -19,8 +17,7 @@ env.hosts = ['18.204.14.158', '54.82.197.53']
 #         archive_path (str): Path to the archive file
 
 #     Returns:
-#         True if all operations ha
-#            ve been done correctly, otherwise returns False
+#         True if all operations have been done correctly, otherwise returns False
 #     """
 #     # Check if the archive file exists
 #     if not path.exists(archive_path):
@@ -62,29 +59,27 @@ env.hosts = ['18.204.14.158', '54.82.197.53']
 #         print(e)
 #         return False
 
-
 def do_deploy(archive_path):
-    """
-    distributes an archive to your web servers
-    """
-    # verificamos si el path existe
-    if os.path.exists(archive_path) is False:
-        return(False)
+    """distributes an archive to webservers"""
+    if not os.path.exists(archive_path):
+        return False
+
+    basename = os.path.basename(archive_path)
+    rem_archive_path = f"/tmp/{basename}"
+
     try:
-        put(archive_path, '/tmp/')
-        _filename = archive_path.split("/")[-1]
-        filename = _filename.split(".")[0]
-        run('mkdir -p /data/web_static/releases/{}'.format(filename))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
-            (_filename, filename))
-        run('rm /tmp/{}'.format(_filename))
-        run('mv -u /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(filename, filename))
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(filename))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{} /data/web_static/current'
-            .format(filename))
-        return(True)
+        put(archive_path, rem_archive_path)
+        x_archive = "/data/web_static/releases/{}".format(
+            os.path.splitext(basename)[0]
+        )
+        run(f"mkdir -p {x_archive}")
+        run("tar -xzf {} -C {} --strip-components=1".format(
+            rem_archive_path, x_archive
+        ))
+        run(f"rm -f {rem_archive_path}")
+        symlink = "/data/web_static/current"
+        run(f"rm -rf {symlink}")
+        run(f"ln -s {x_archive}/ {symlink}")
     except Exception:
-        return(False)
+        return False
+    return True
